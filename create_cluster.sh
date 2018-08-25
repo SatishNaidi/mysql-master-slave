5c3c2931c93c2c796e94a099df2a93dd435d7c14
Naidi <SatishKumar.Naidi@ca.com>
2018-08-25 10:21:25 +0530

    add create_cluster script

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,72 +1,72 @@
 #!/bin/bash
+# username password master_host slave_host
+#create_cluster.sh root f00bar ec2-34-207-211-209.compute-1.amazonaws.com ec2-35-153-52-32.compute-1.amazonaws.com
+# USER=root
+# PASS=f00bar
+# MASTER_HOST=ec2-34-207-211-209.compute-1.amazonaws.com
+# SLAVE_HOSTS=ec2-35-153-52-32.compute-1.amazonaws.com
+ 
+USER=${1}
+PASS=${2}
+MASTER_HOST=${3}
+SLAVE_HOST=${4}
 
-/usr/local/bin/docker-compose down
-rm -rf ./master/data/*
-rm -rf ./slave/data/*
-/usr/local/bin/docker-compose build
-/usr/local/bin/docker-compose up -d
+MYSQL_M_PORT=3002
+MYSQL_S_PORT=3003
+DB=mydb
+DUMP_FILE="/tmp/$DB-export-$(date +"%Y%m%d%H%M%S").sql" 
 
+echo "MASTER: $MASTER_HOST"
+ 
+mysql -h $MASTER_HOST -P ${MYSQL_M_PORT} "-u$USER" "-p$PASS" $DB <<-EOSQL &
+	GRANT REPLICATION SLAVE ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';
+	FLUSH PRIVILEGES;
+	FLUSH TABLES WITH READ LOCK;
+	DO SLEEP(3600);
+EOSQL
+ 
+echo "  - Waiting for database to be locked"
+sleep 3
+ 
+echo "  - Dumping database to $DUMP_FILE"
+mysqldump -h $MASTER_HOST -P ${MYSQL_M_PORT} "-u$USER" "-p$PASS" --opt $DB > $DUMP_FILE
+echo "  - Dump complete."
+ 
+MASTER_STATUS=$(mysql -h $MASTER_HOST -P ${MYSQL_M_PORT} "-u$USER" "-p$PASS" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
+LOG_FILE=$(echo $MASTER_STATUS | cut -f1 -d ' ')
+LOG_POS=$(echo $MASTER_STATUS | cut -f2 -d ' ')
+echo "  - Current log file is $LOG_FILE and log position is $LOG_POS"
+ 
 
+kill $! 2>/dev/null
+wait $! 2>/dev/null
+ 
+echo "  - Master database unlocked"
+ 
+echo "SLAVE: $SLAVE_HOST"
+USER=root
+echo "  - Creating database copy"
+mysql -h $SLAVE_HOST -P ${MYSQL_S_PORT} "-u$USER" "-p$PASS" -e "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB;"
 
-#export $(grep -v '^#' master/mysql_master.env  | xargs)
+mysql -h $SLAVE_HOST -P ${MYSQL_S_PORT} "-u$USER" "-p$PASS" $DB < $DUMP_FILE
 
-#MYSQL_M_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-#MYSQL_M_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-#MYSQL_M_USER=${MYSQL_USER}
-#MYSQL_M_PASSWORD=${MYSQL_PASSWORD}
-#MYSQL_M_DATABASE=${MYSQL_DATABASE}
+echo "  - Setting up slave replication"
+mysql -h $SLAVE_HOST -P ${MYSQL_S_PORT} "-u$USER" "-p$PASS" $DB <<-EOSQL &
+	STOP SLAVE;
+	CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',
+	MASTER_PORT=3002,
+	MASTER_USER='$USER',
+	MASTER_PASSWORD='f00bar',
+	MASTER_LOG_FILE='$LOG_FILE',
+	MASTER_LOG_POS=$LOG_POS;
+	START SLAVE;
+EOSQL
 
-#export $(grep -v '^#' slave/mysql_slave.env | xargs)
+sleep 2
 
-#MYSQL_S_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-#MYSQL_S_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-#MYSQL_S_USER=${MYSQL_USER}
-#MYSQL_S_PASSWORD=${MYSQL_PASSWORD}
-#MYSQL_S_DATABASE=${MYSQL_DATABASE}
-
-
-
-#export $(grep -v '^#' master/mysql_master.env  | xargs)
-
-MYSQL_M_ROOT_PASSWORD=MyPassword12
-MYSQL_M_USER=mydb_user
-MYSQL_M_PASSWORD=mydb_pwd
-MYSQL_M_DATABASE=mydb
-
-#export $(grep -v '^#' slave/mysql_slave.env | xargs)
-
-MYSQL_S_ROOT_PASSWORD=MyPWD123
-MYSQL_S_USER=mydb_slave_user1
-MYSQL_S_PASSWORD=mydb_slave_pwd1
-MYSQL_S_DATABASE=mydb
-
-until docker exec mysql_master sh -c 'export MYSQL_PWD='${MYSQL_M_ROOT_PASSWORD}'; mysql -u root -e ";"'
-do
-    echo "Waiting for mysql_master database connection..."
-    sleep 4
-done
-
-priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "'${MYSQL_S_USER}'"@"%" IDENTIFIED BY "'${MYSQL_S_PASSWORD}'"; FLUSH PRIVILEGES;'
-docker exec mysql_master sh -c "export MYSQL_PWD='${MYSQL_M_ROOT_PASSWORD}'; mysql -u root -e '$priv_stmt'"
-
-until /usr/local/bin/docker-compose exec mysql_slave sh -c 'export MYSQL_PWD='${MYSQL_S_ROOT_PASSWORD}'; mysql -u root -e ";"'
-do
-    echo "Waiting for mysql_slave database connection..."
-    sleep 4
-done
-
-docker-ip() {
-    docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
-}
-
-MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD='${MYSQL_M_ROOT_PASSWORD}'; mysql -u root -e "SHOW MASTER STATUS"'`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
-
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='"${MYSQL_S_USER}"',MASTER_PASSWORD='"${MYSQL_S_PASSWORD}"',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-start_slave_cmd='export MYSQL_PWD='${MYSQL_S_ROOT_PASSWORD}'; mysql -u root -e "'
-start_slave_cmd+="$start_slave_stmt"
-start_slave_cmd+='"'
-docker exec mysql_slave sh -c "$start_slave_cmd"
-
-docker exec mysql_slave sh -c "export MYSQL_PWD='${MYSQL_S_ROOT_PASSWORD}'; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+SLAVE_OK=$(mysql -h $SLAVE_HOST -P ${MYSQL_S_PORT} "-u$USER" "-p$PASS" -e "SHOW SLAVE STATUS\G;" | grep 'Waiting for master')
+if [ -z "$SLAVE_OK" ]; then
+	echo "  - Error ! Wrong slave IO state."
+else
+	echo "  - Slave IO state OK"
+fi
